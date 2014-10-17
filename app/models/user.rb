f0cef82795330be052e30aa862eaca8a38ec5c82
paoloferraro99 <paoloferraro99@gmail.com>
2014-10-17 19:01:49 +0000

    Created auth for guest,user,mod,admin.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
-		 
-  has_many :posts
+	# Include default devise modules. Others available are:
+	# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+	:recoverable, :rememberable, :trackable, :validatable, :confirmable
+	
+	has_many :posts
+	
+	def admin?
+		role == 'admin'
+	end
+	
+	def moderator?
+		role == 'moderator'
+	end
 end
