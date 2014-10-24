305638ba2ab39de1d33623aaf20deee7c8bc9501
paoloferraro99 <paoloferraro99@gmail.com>
2014-10-24 17:59:10 +0000

    Validated posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,14 @@ module ApplicationHelper
 def my_name
   "Paolo F."
   end
+  
+  def form_group_tag(errors, &block)
+     if errors.any?
+       content_tag :div, capture(&block), class: 'form-group has-error'
+     else
+       content_tag :div, capture(&block), class: 'form-group'
+
+     end
+   end
 
 end
