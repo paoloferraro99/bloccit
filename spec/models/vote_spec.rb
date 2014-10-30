5913d63e9c75084aa2f53d8b595f9707faef3e51
paoloferraro99 <paoloferraro99@gmail.com>
2014-10-30 02:07:25 +0000

    installed rspec

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,16 @@
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        v = Vote.new(value: 1)
+		expect( v.valid? ).to eq(true)
+
+		v2 = Vote.new(value: -1)
+		expect( v2.valid? ).to eq(true)
+
+		v3 = Vote.new(value: 2)
+		expect( v3.valid? ).to eq(true)
+      end
+    end
+  end
+end
